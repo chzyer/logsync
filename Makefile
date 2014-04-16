9a060d050c00d007afc3cb5347a540bfe4e66e27
Cheney <me@chenye.org>
2014-04-16 17:25:23 +0800

    update eventlog

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
 export GOPATH := $(shell pwd)
 pwd := $(shell pwd)
 $(shell rm -fr pkg)
-all:server client
+all:server client eventlog
 
 server:
 	go build -o bin/server service/server.go
 
 client:
 	go build -o bin/client service/client.go
+
+eventlog:
+	go build -o bin/eventlog service/eventlog.go
--- service/eventlog.go
+++ service/eventlog.go
@@ -20,7 +20,13 @@ func main() {
 	for {
 	select {
 	case ev := <-watcher.Event:
-		log.Println("[INFO]", ev)
+		switch {
+		case ev.Match(inotify.IN_OPEN):
+		case ev.Match(inotify.IN_CLOSE):
+		case ev.Match(inotify.IN_ACCESS):
+		default:
+			log.Println("[INFO]", ev)
+		}
 	case err := <-watcher.Error:
 		log.Println("[ERROR]", err)
 	}
